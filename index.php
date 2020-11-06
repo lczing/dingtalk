2c0dfa9a2b11620e89ee751ecc194350b5a09b99
李昌 <lichang1@qimai.cn>
2020-11-06 19:29:20 +0800

    add4

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include './src/Robots.php';
-
-$access_token = 'your acces token';
-$secret = 'your secret';
-
-$ro = new Lczing\DingTalk\Robots($access_token, $secret);
-$ro->dingMsg('your msg');
